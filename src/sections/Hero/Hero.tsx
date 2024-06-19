96c4ff46aa57990fc26e24d165186550fc564bc4
Helen Ho <hth2016@stern.nyu.edu>
2024-06-19 00:03:48 -0400

    ver jun 19

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,12 +1,12 @@
-import styles from './HeroStyles.module.css'
-import heroImg from "../../assets/headshot.png"
-import sun from "../../assets/sun.svg"
-import moon from "../../assets/moon.svg"
-import linkedinLight from "../../assets/linkedin-light.svg"
-import linkedinDark from "../../assets/linkedin-dark.svg"
-import githubLight from "../../assets/github-light.svg"
-import githubDark from "../../assets/github-dark.svg"
-import CV from "../../assets/cv.pdf"
+import styles from './HeroStyles.module.css';
+import headshot from "../../assets/headshot.png";
+import sun from "../../assets/sun.svg";
+import moon from "../../assets/moon.svg";
+import linkedinLight from "../../assets/linkedin-light.svg";
+import linkedinDark from "../../assets/linkedin-dark.svg";
+import githubLight from "../../assets/github-light.svg";
+import githubDark from "../../assets/github-dark.svg";
+import CV from "../../assets/cv.pdf";
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
@@ -20,9 +20,10 @@ function Hero() {
     <section id="hero" className={styles.container}>
       <div className={styles.colorModeContainer}>
         <img
-          src={heroImg}
+          src={headshot}
           className={styles.hero}
-          alt="Profile picture of Harris Johnsen"
+          alt="Profile picture of Helen Ho"
+          style={{ width: '300px', height: '300px' }}
         />
         <img
           className={styles.colorMode}
@@ -33,22 +34,21 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Harris
+          Helen
           <br />
-          Johnsen
+          Ho
         </h1>
-        <h2>Frontend Developer</h2>
+        <h2>Software Engineer</h2>
         <span>
-          <a href="https://github.com/hhelenho" target="_blank">
+          <a href="https://github.com/hhelenho" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon} alt="Github icon" />
           </a>
-          <a href="https://www.linkedin.com/in/hhelenho/" target="_blank">
+          <a href="https://www.linkedin.com/in/hhelenho/" target="_blank" rel="noopener noreferrer">
             <img src={linkedinIcon} alt="Linkedin icon" />
           </a>
         </span>
         <p className={styles.description}>
-          With a passion for developing modern React web apps for commercial
-          businesses.
+          Finance & CS Undergraduate Student at New York University, Product Manager & Data Engineer
         </p>
         <a href={CV} download>
           <button className="hover">Resume</button>
