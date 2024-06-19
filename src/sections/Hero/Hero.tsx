24f4f932eac4fda8758760c7936ec44e124cfff7
Helen Ho <hth2016@stern.nyu.edu>
2024-06-18 23:40:48 -0400

    version jun 18

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -1,10 +1,49 @@
 import styles from './HeroStyles.module.css'
 import heroImg from "../../assets/headshot.png"
+import themeIcon from "../../assets/sun.svg"
+import linkedinIcon from "../../assets/linkedin-light.svg"
+import githubIcon from "../../assets/github-light.svg"
+import CV from "../../assets/cv.pdf"
 
 function Hero() {
   return (
-    <section id="hero">
-      <div><img src={heroImg} alt="" /></div>
+    <section id="hero" className={styles.container}>
+      <div className = {styles.colorModeContainer}>
+        <img className={styles.hero}
+      src={heroImg} 
+      alt="Profile picture of Helen Ho" 
+      />
+      <img 
+      className = {styles.collorMode}
+      src={themeIcon} 
+      alt="Color mode icon"
+      />
+        </div>
+        <div className={styles.info}/>
+        <h1>
+          Helen
+          <br />
+          Ho
+        </h1>
+        <h2>
+          Frontend Developer
+        </h2>
+        <span>
+          <a href="https://www.linkedin.com/in/hhelenho/" target="_blank">
+            <img src={linkedinIcon} alt="LinkedIn icon"/>
+          </a>
+          <a href="https://github.com/hhelenho" target="_blank">
+            <img src={githubIcon} alt="Github icon"/>
+          </a>
+        </span>
+        <p>
+          Finance & Computer Science Student at New York University, Product Manager and Data Engineer
+        </p>
+        <a href={CV}>
+          <button className="hover" download>
+            Resume
+          </button>
+        </a>
     </section>
   )
 }
