e183cbad78c380f5a1d4a270ad3c09cbdd2d9ca0
Helen Ho <hth2016@stern.nyu.edu>
2024-06-19 00:07:30 -0400

    ver jun 19

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -23,7 +23,7 @@ function Hero() {
           src={headshot}
           className={styles.hero}
           alt="Profile picture of Helen Ho"
-          style={{ width: '300px', height: '300px' }}
+          style={{ width: '300px', height: '309px' }}
         />
         <img
           className={styles.colorMode}
@@ -34,11 +34,8 @@ function Hero() {
       </div>
       <div className={styles.info}>
         <h1>
-          Helen
-          <br />
-          Ho
+          Helen Ho
         </h1>
-        <h2>Software Engineer</h2>
         <span>
           <a href="https://github.com/hhelenho" target="_blank" rel="noopener noreferrer">
             <img src={githubIcon} alt="Github icon" />
